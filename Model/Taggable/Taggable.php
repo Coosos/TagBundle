00bdf3b4df76172908d280d8cfc0fc3261c85573
Rémy Lescallier <lescallier1@gmail.com>
2017-06-19 11:57:34 +0200

    Fix major error

--- Model/Taggable/Taggable.php
+++ Model/Taggable/Taggable.php
@@ -2,11 +2,9 @@
 
 namespace Coosos\TagBundle\Model\Taggable;
 
-use Coosos\TagBundle\Model\Taggable as Tag;
-
 trait Taggable {
 
-    use Tag\TaggableProperties,
-        Tag\TaggableMethods;
+    use TaggableProperties;
+    use TaggableMethods;
 
 }
