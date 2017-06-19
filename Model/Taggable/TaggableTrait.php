7b954bfcd535db9aab52546a4b8860c38005eb6f
Rémy Lescallier <lescallier1@gmail.com>
2017-06-19 13:41:09 +0200

    Fix major error

--- Model/Taggable/TaggableTrait.php
+++ Model/Taggable/TaggableTrait.php
@@ -2,10 +2,49 @@
 
 namespace Coosos\TagBundle\Model\Taggable;
 
-use Coosos\TagBundle\Model\Taggable as tag;
+use Coosos\TagBundle\Entity\Tag;
+use Doctrine\Common\Collections\ArrayCollection;
 
 trait TaggableTrait
 {
-    use tag\TaggablePropertiesTrait;
-    use tag\TaggableMethodsTrait;
+    /**
+     * @var ArrayCollection
+     *
+     * @ORM\ManyToMany(targetEntity="Coosos\TagBundle\Entity\Tag", cascade={"persist"})
+     */
+    protected $tags;
+
+    /**
+     * @param Tag $tag
+     * @return $this
+     */
+    public function addTag(Tag $tag)
+    {
+        if (!$this->tags) { $this->tags = new ArrayCollection(); }
+
+        $this->tags->add($tag);
+
+        return $this;
+    }
+
+    /**
+     * @param Tag $tag
+     * @return $this
+     */
+    public function removeTag(Tag $tag)
+    {
+        if (!$this->tags) { $this->tags = new ArrayCollection($this->tags); }
+
+        $this->tags->remove($tag);
+
+        return $this;
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getTags()
+    {
+        return ($this->tags) ? $this->tags : new ArrayCollection();
+    }
 }
